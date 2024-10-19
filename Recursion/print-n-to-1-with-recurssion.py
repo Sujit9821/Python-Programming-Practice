15b27742f1b643b07f90e17f88540e180c78cff3
Sujit <sks.official9821@gmail.com>
2024-10-19 17:52:49 +0530

    print n to 1 with recursion

---
+++ Recursion/print-n-to-1-with-recurssion.py
@@ -0,0 +1,13 @@
+def print_r(n):
+    if n==1:
+        print(n)
+        return 1
+    print(n)
+    print_r(n-1)
+    
+if __name__ == "__main__":
+    n = int(input("Give upto which you want to print: "))
+    if n<=0:
+        print("User should provide value more than 0.")
+    else:
+        print_r(n)
